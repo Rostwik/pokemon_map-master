17be0a71bbb188fe34db61781e56bf459daab852
Rostislav <rostwik@gmail.com>
2022-01-06 15:14:36 +0300

    show pokemons on mainpage map

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -47,7 +47,7 @@ def show_all_pokemons(request):
         })
 
     return render(request, 'mainpage.html', context={
-        'map': folium_map._repr_html (),
+        'map': folium_map._repr_html_(),
         'pokemons': pokemons_on_page,
     })
 
