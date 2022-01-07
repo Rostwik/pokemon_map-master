f376b450faa1d8176880594ddf9a8c11bcc8c980
Rostislav <rostwik@gmail.com>
2022-01-07 21:16:13 +0300

    Configured the required parameters of the models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,9 +4,9 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name='Наименование')
     image = models.ImageField(blank=True, verbose_name='Изображение Покемона')
-    description = models.TextField(null=True, verbose_name='Описание')
-    title_en = models.CharField(max_length=100, null=True, verbose_name='Наименование англ.')
-    title_jp = models.CharField(max_length=100, null=True, verbose_name='Наименование яп.')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    title_en = models.CharField(max_length=100, blank=True, verbose_name='Наименование англ.')
+    title_jp = models.CharField(max_length=100, blank=True, verbose_name='Наименование яп.')
     previous_evolution = models.ForeignKey(
         'self',
         null=True,
@@ -22,8 +22,8 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
-    lat = models.FloatField(null=True, verbose_name='Широта')
-    lon = models.FloatField(null=True, verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта')
+    lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(null=True, blank=True, verbose_name='Дата и время появляения')
     disappeared_at = models.DateTimeField(null=True, blank=True, verbose_name='Дата и время исчезновения')
     level = models.IntegerField(null=True, blank=True, verbose_name='Уровень')
